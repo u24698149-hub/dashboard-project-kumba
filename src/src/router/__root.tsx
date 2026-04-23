fc22dd83ada0c99f9549155b39572a1a3fa345a2
u24698149-hub <u24698149@tuks.co.za>
2026-04-23 03:14:20 +0200

    Delete src/src/router directory

--- src/src/router/__root.tsx
+++
@@ -1,6 +0,0 @@
-import { createRootRoute } from '@tanstack/react-router'
-import { Outlet } from '@tanstack/react-router'
-
-export const Route = createRootRoute({
-  component: () => <Outlet />,
-})
